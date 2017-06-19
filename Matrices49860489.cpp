df3aa572e51565daf056e4b373b23e8d72f4eec3
Dobless <jacky9491@naver.com>
2017-06-19 23:53:06 +0900

    6.19 - effect modify

--- Matrices49860489.cpp
+++ Matrices49860489.cpp
@@ -5,43 +5,37 @@
 #include <d3dx9.h>
 #include <iostream>
 
-// 음악 관련
 #include <mmsystem.h>
-#include <strmif.h>
-#include <control.h>
-#include <uuids.h>
+#include <Digitalv.h>
 
 // define the screen resolution and keyboard macros
-#define SCREEN_WIDTH  400
-#define SCREEN_HEIGHT 380
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 760
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 #define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
 
 #define ENEMY_NUM 10 
 #define BULLET_NUM 10
 
+bool Ui = true;
+bool Ingame = false;
 
 // include the Direct3D Library file
 #pragma comment (lib, "d3d9.lib")
 #pragma comment (lib, "d3dx9.lib")
-
-// 음악 관련
-#pragma comment(lib, "strmiids.lib")
+#pragma comment (lib, "winmm.lib")
 
 // global declarations
 LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
 LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
 LPD3DXSPRITE d3dspt;    // the pointer to our Direct3D Sprite interface
 
-
-
 						// sprite declarations
 LPDIRECT3DTEXTURE9 sprite;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_hero;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_enemy;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_bullet;    // the pointer to the sprite
-
-
+LPDIRECT3DTEXTURE9 sprite_effect;
 
 									 // function prototypes
 void initD3D(HWND hWnd);    // sets up and initializes Direct3D
@@ -51,6 +45,17 @@ void cleanD3D(void);		// closes Direct3D and releases memory
 void init_game(void);
 void do_game_logic(void);
 bool sphere_collision_check(float x0, float y0, float size0, float x1, float y1, float size1);
+
+MCI_OPEN_PARMS mciopen;
+MCI_PLAY_PARMS mciplay;
+int dwID;
+
+DWORD hTime;
+DWORD hNow;
+DWORD eTime;
+DWORD eNow;
+
+bool one;
 
 
 // the WindowProc function prototype
@@ -88,22 +93,45 @@ bool sphere_collision_check(float x0, float y0, float size0, float x1, float y1,
 
 //주인공 클래스 
 class Hero :public entity {
-
 public:
+	bool space;
+
 	void fire();
 	void super_fire();
 	void move(int i);
 	void init(float x, float y);
-
+	void effect(float x, float y);
+	bool effect_show();
+	void effect_active();
+	void effect_hide();
 
 };
 
 void Hero::init(float x, float y)
 {
-
 	x_pos = x;
 	y_pos = y;
+}
 
+void Hero::effect(float x, float y)
+{
+	x_pos = x;
+	y_pos = y;
+}
+
+bool Hero::effect_show()
+{
+	return space;
+}
+
+void Hero::effect_active()
+{
+	space = true;
+}
+
+void Hero::effect_hide()
+{
+	space = false;
 }
 
 void Hero::move(int i)
@@ -118,22 +146,57 @@ void Hero::move(int i)
 		y_pos += 3;
 		break;
 
-
 	case MOVE_LEFT:
 		x_pos -= 3;
 		break;
 
-
 	case MOVE_RIGHT:
 		x_pos += 3;
 		break;
-
 	}
-
 }
 
+class LeftKey :public entity {
+public:
+	bool bleft;
 
+	void fire();
+	void super_fire();
+	void move(int i);
+	void init(float x, float y);
+	void effect(float x, float y);
+	bool effect_show();
+	void effect_active();
+	void effect_hide();
 
+};
+
+void LeftKey::init(float x, float y)
+{
+	x_pos = x;
+	y_pos = y;
+}
+
+void LeftKey::effect(float x, float y)
+{
+	x_pos = x;
+	y_pos = y;
+}
+
+bool LeftKey::effect_show()
+{
+	return bleft;
+}
+
+void LeftKey::effect_active()
+{
+	bleft = true;
+}
+
+void LeftKey::effect_hide()
+{
+	bleft = false;
+}
 
 // 적 클래스 
 class Enemy :public entity {
@@ -143,26 +206,48 @@ public:
 	void init(float x, float y);
 	void move();
 
+	float ex_pos;
+	float ey_pos;
+	
+	bool hit;
+	void effect(float x, float y);
+	bool effect_show();
+	void effect_active();
+	void effect_hide();
 };
 
 void Enemy::init(float x, float y)
 {
-
 	x_pos = x;
 	y_pos = y;
-
 }
-
 
 void Enemy::move()
 {
 	y_pos += 2;
+}
+
+void Enemy::effect(float x, float y)
+{
+	ex_pos = x;
+	ey_pos = y;
+}
+
+bool Enemy::effect_show()
+{
+	return hit;
+}
+
+void Enemy::effect_active()
+{
+	hit = true;
 
 }
 
-
-
-
+void Enemy::effect_hide()
+{
+	hit = false;
+}
 
 
 // 총알 클래스 
@@ -177,14 +262,10 @@ public:
 	void hide();
 	void active();
 	bool check_collision(float x, float y);
-
-
 };
-
 
 bool Bullet::check_collision(float x, float y)
 {
-
 	//충돌 처리 시 
 	if (sphere_collision_check(x_pos, y_pos, 32, x, y, 32) == true)
 	{
@@ -192,37 +273,25 @@ bool Bullet::check_collision(float x, float y)
 		return true;
 	}
 	else {
-
 		return false;
 	}
 }
-
-
-
 
 void Bullet::init(float x, float y)
 {
 	x_pos = x;
 	y_pos = y;
-
 }
-
-
 
 bool Bullet::show()
 {
 	return bShow;
-
 }
-
 
 void Bullet::active()
 {
 	bShow = true;
-
 }
-
-
 
 void Bullet::move()
 {
@@ -232,270 +301,8 @@ void Bullet::move()
 void Bullet::hide()
 {
 	bShow = false;
-
 }
 
-// 음악 관련
-class Mp3
-{
-public:
-	Mp3();
-	~Mp3();
-
-	bool Load(LPCWSTR filename);
-	void Cleanup();
-
-	bool Play();
-	bool Pause();
-	bool Stop();
-
-	// msTirmeout이 0이되면 즉시반환
-	// mp3가 끝나면 true 반환
-	bool WaitForCompletion(long msTimeout, long *EvCode);
-
-	// 볼륨 조절
-	bool SetVolume(long vol);
-	long GetVolume();
-
-	// 1억분의 1초단위로 시간 반환
-	// 10,000,000 == 1초
-	// 결과를 1억으로 나눔
-	// 초단위의 지속시간
-	__int64 GetDuration();
-
-	// 현재 재생 위치 반환
-	__int64 GetCurrentPosition();
-
-	//
-	bool SetPositions(__int64* pCurrent, __int64* pStop, bool bAbsolutePositioning);
-
-private:
-	IGraphBuilder *  pigb;
-	IMediaControl *  pimc;
-	IMediaEventEx *  pimex;
-	IBasicAudio * piba;
-	IMediaSeeking * pims;
-	bool ready;
-
-	// mp3의 지속시간
-	__int64 duration;
-};
-
-Mp3::Mp3()
-{
-	pigb = NULL;
-	pimc = NULL;
-	pimex = NULL;
-	piba = NULL;
-	pims = NULL;
-	ready = false;
-	duration = 0;
-}
-
-Mp3::~Mp3()
-{
-	Cleanup();
-}
-
-void Mp3::Cleanup()
-{
-	if (pimc) pimc->Stop();
-	
-	if (pigb)
-	{
-		pigb->Release();
-		pigb = NULL;
-	}
-
-	if (pimc)
-	{
-		pimc->Release();
-		pimc = NULL;
-	}
-
-	if (pimex)
-	{
-		pimex->Release();
-		pimex = NULL;
-	}
-
-	if (piba)
-	{
-		piba->Release();
-		piba = NULL;
-	}
-
-	if (pims)
-	{
-		pims->Release();
-		pims = NULL;
-	}
-
-	ready = false;
-}
-
-bool Mp3::Load(LPCWSTR szFile)
-{
-	Cleanup();
-	ready = false;
-
-	if (SUCCEEDED(CoCreateInstance(CLSID_FilterGraph,
-		NULL,
-		CLSCTX_INPROC_SERVER,
-		IID_IGraphBuilder,
-		(void **)&this->pigb)))
-	{
-		pigb->QueryInterface(IID_IMediaControl, (void **)&pimc);
-		pigb->QueryInterface(IID_IMediaEventEx, (void **)&pimex);
-		pigb->QueryInterface(IID_IBasicAudio, (void**)&piba);
-		pigb->QueryInterface(IID_IMediaSeeking, (void**)&pims);
-
-		HRESULT hr = pigb->RenderFile(szFile, NULL);
-		if (SUCCEEDED(hr))
-		{
-			ready = true;
-			if (pims)
-			{
-				pims->SetTimeFormat(&TIME_FORMAT_MEDIA_TIME);
-				pims->GetDuration(&duration); // returns 10,000,000 for a second.
-				duration = duration;
-			}
-		}
-	}
-
-	return ready;
-}
-
-bool Mp3::Pause()
-{
-	if (ready&&pimc)
-	{
-		HRESULT hr = pimc->Pause();
-		return SUCCEEDED(hr);
-	}
-
-	return false;
-}
-
-bool Mp3::Stop()
-{
-	if (ready&&pimc)
-	{
-		HRESULT hr = pimc->Stop();
-		return SUCCEEDED(hr);
-	}
-
-	return false;
-}
-
-bool Mp3::WaitForCompletion(long msTimeout, long* EvCode)
-{
-	if (ready&&pimex)
-	{
-		HRESULT hr = pimex->WaitForCompletion(msTimeout, EvCode);
-		return *EvCode > 0;
-	}
-
-	return false;
-}
-
-bool Mp3::SetVolume(long vol)
-{
-	if (ready&&piba)
-	{
-		HRESULT hr = piba->put_Volume(vol);
-		return SUCCEEDED(hr);
-	}
-
-	return false;
-}
-
-long Mp3::GetVolume()
-{
-	if (ready&&piba)
-	{
-		long vol = -1;
-		HRESULT hr = piba->get_Volume(&vol);
-
-		if (SUCCEEDED(hr))
-			return vol;
-	}
-
-	return -1;
-}
-
-__int64 Mp3::GetDuration()
-{
-	return duration;
-}
-
-__int64 Mp3::GetCurrentPosition()
-{
-	if (ready&&pims)
-	{
-		__int64 curpos = -1;
-		HRESULT hr = pims->GetCurrentPosition(&curpos);
-
-		if (SUCCEEDED(hr))
-			return curpos;
-	}
-
-	return -1;
-}
-
-bool Mp3::SetPositions(__int64* pCurrent, __int64* pStop, bool bAbsolutePositioning)
-{
-	if (ready&&pims)
-	{
-		DWORD flags = 0;
-		if (bAbsolutePositioning)
-			flags = AM_SEEKING_AbsolutePositioning | AM_SEEKING_SeekToKeyFrame;
-		else
-			flags = AM_SEEKING_RelativePositioning | AM_SEEKING_SeekToKeyFrame;
-
-		HRESULT hr = pims->SetPositions(pCurrent, flags, pStop, flags);
-
-		if (SUCCEEDED(hr))
-			return true;
-	}
-
-	return false;
-}
-
-class CLibMP3DLL
-{
-public:
-	CLibMP3DLL(void);
-	~CLibMP3DLL(void);
-
-	bool LoadDLL(LPCWSTR dll);
-	void UnloadDLL();
-
-	bool Load(LPCWSTR filename);
-	bool Cleanup();
-
-	bool Play();
-	bool Pause();
-	bool Stop();
-	bool WaitForCompletion(long msTimeout, long* EvCode);
-
-	bool SetVolume(long vol);
-	long GetVolume();
-
-	__int64 GetDuration();
-	__int64 GetCurrentPosition();
-
-	bool SetPositions(__int64* pCurrent, __int64* pStop, bool bAbsolutePositioning);
-
-
-private:
-	HMODULE m_Mod;
-};
-
-
-
-
-//객체 생성 
 Hero hero;
 Enemy enemy[ENEMY_NUM];
 Bullet bullet[BULLET_NUM];
@@ -521,7 +328,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	RegisterClassEx(&wc);
 
 	hWnd = CreateWindowEx(NULL, L"WindowClass", L"Our Direct3D Program",
-		WS_EX_TOPMOST | WS_POPUP, 1000, 300, SCREEN_WIDTH, SCREEN_HEIGHT,
+		WS_EX_TOPMOST | WS_POPUP, 600, 400, SCREEN_WIDTH, SCREEN_HEIGHT,
 		NULL, NULL, hInstance, NULL);
 
 	ShowWindow(hWnd, nCmdShow);
@@ -536,6 +343,16 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	// enter the main loop:
 
 	MSG msg;
+
+	mciopen.lpstrElementName = L"Happy_things.mp3";
+	mciopen.lpstrDeviceType = L"MPEGVideo";
+
+	mciSendCommand(dwID, MCI_OPEN, MCI_OPEN_TYPE | MCI_OPEN_ELEMENT, (DWORD)(LPVOID)&mciopen);
+
+	dwID = mciopen.wDeviceID;
+
+	mciSendCommand(1, MCI_PLAY, MCI_NOTIFY, (DWORD)(LPVOID)&mciplay);
+
 
 	while (TRUE)
 	{
@@ -557,9 +374,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		// check the 'escape' key
 		if (KEY_DOWN(VK_ESCAPE))
 			PostMessage(hWnd, WM_DESTROY, 0, 0);
-
-
-
 
 		while ((GetTickCount() - starting_point) < 25);
 	}
@@ -613,9 +427,9 @@ void initD3D(HWND hWnd)
 	D3DXCreateSprite(d3ddev, &d3dspt);    // create the Direct3D Sprite object
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
-		L"Panel3.png",    // the file name
-		D3DX_DEFAULT,    // default width
-		D3DX_DEFAULT,    // default height
+		L"background.png",    // the file name
+		800,    // default width
+		760,    // default height
 		D3DX_DEFAULT,    // no mip mapping
 		NULL,    // regular usage
 		D3DFMT_A8R8G8B8,    // 32-bit pixels with alpha
@@ -629,6 +443,21 @@ void initD3D(HWND hWnd)
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
 		L"hero.png",    // the file name
+		64,    // default width
+		64,    // default height
+		D3DX_DEFAULT,    // no mip mapping
+		NULL,    // regular usage
+		D3DFMT_A8R8G8B8,    // 32-bit pixels with alpha
+		D3DPOOL_MANAGED,    // typical memory handling
+		D3DX_DEFAULT,    // no filtering
+		D3DX_DEFAULT,    // no mip filtering
+		D3DCOLOR_XRGB(255, 0, 255),    // the hot-pink color key
+		NULL,    // no image info struct
+		NULL,    // not using 256 colors
+		&sprite_hero);    // load to sprite
+
+	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
+		L"effect.png",    // the file name
 		D3DX_DEFAULT,    // default width
 		D3DX_DEFAULT,    // default height
 		D3DX_DEFAULT,    // no mip mapping
@@ -640,7 +469,7 @@ void initD3D(HWND hWnd)
 		D3DCOLOR_XRGB(255, 0, 255),    // the hot-pink color key
 		NULL,    // no image info struct
 		NULL,    // not using 256 colors
-		&sprite_hero);    // load to sprite
+		&sprite_effect);    // load to sprite
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
 		L"enemy.png",    // the file name
@@ -680,11 +509,14 @@ void init_game(void)
 {
 	//객체 초기화 
 	hero.init(150, 300);
+	hero.effect(hero.x_pos, hero.y_pos);
+	one = true;
 
 	//적들 초기화 
 	for (int i = 0; i<ENEMY_NUM; i++)
 	{
 		enemy[i].init((float)(rand() % 300), rand() % 200 - 300);
+		enemy[i].effect(enemy[i].x_pos, enemy[i].y_pos);
 	}
 
 	//총알 초기화 
@@ -712,6 +544,11 @@ void do_game_logic(void)
 	if (KEY_DOWN(VK_RIGHT))
 		hero.move(MOVE_RIGHT);
 
+	if (KEY_DOWN(VK_SPACE))
+	{
+		hero.effect_active();
+		hero.effect(hero.x_pos, hero.y_pos);
+	}
 
 	//적들 처리 
 	for (int i = 0; i<ENEMY_NUM; i++)
@@ -722,108 +559,162 @@ void do_game_logic(void)
 			enemy[i].move();
 	}
 
-
 	//총알 처리
-	for (int i = 0; i<ENEMY_NUM; i++)
+	if (KEY_DOWN(VK_SPACE))
 	{
-		if (bullet[i].show() == false)
+		if (one)
 		{
-			if (KEY_DOWN(VK_SPACE))
-			{
-				bullet[i].active();
-				bullet[i].init(hero.x_pos, hero.y_pos);
-			}
-		}
-		if (bullet[i].show() == true)
-		{
-			if (bullet[i].y_pos < -70) bullet[i].hide();
-			else bullet[i].move();
-
-			//충돌 처리 
 			for (int i = 0; i < BULLET_NUM; i++)
 			{
-				if (bullet[i].check_collision(enemy[i].x_pos, enemy[i].y_pos) == true)
+				if (bullet[i].show() == false)
 				{
-					for (int i = 0; i < ENEMY_NUM; i++)
-					{
-						enemy[i].init((float)(rand() % 300), rand() % 200 - 300);
-					}
+					bullet[i].active();
+					bullet[i].init(hero.x_pos, hero.y_pos);
+					hTime = timeGetTime() / 1000;
+					one = false;
+					break;
 				}
 			}
 		}
 	}
+	if (KEY_UP(VK_SPACE))
+	{
+		one = true;
+	}
+	
+	for (int i = 0; i < BULLET_NUM; i++)
+	{
+		if (bullet[i].show() == true)
+		{
+			if (bullet[i].y_pos < -70) bullet[i].hide();
+			else bullet[i].move();
+			
+			//충돌 처리 
+			for (int y = 0; y < ENEMY_NUM; y++)
+			{
+				if (bullet[i].check_collision(enemy[y].x_pos, enemy[y].y_pos) == true)
+				{
+					enemy[y].effect(enemy[y].x_pos, enemy[y].y_pos);
+					enemy[y].effect_active();
+					eTime = timeGetTime() / 1000;
+
+					enemy[y].init((float)(rand() % 300), rand() % 200 - 300);
+					break;
+				}
+			}
+		}
+	}
+	
 }
 
 // this is the function used to render a single frame
 void render_frame(void)
 {
-	// clear the window to a deep blue
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(116, 167, 243), 1.0f, 0);
 
-	d3ddev->BeginScene();    // begins the 3D scene
+	//if (Ingame)
+	//{
+		// clear the window to a deep blue
+		d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 
-	d3dspt->Begin(D3DXSPRITE_ALPHABLEND);    // // begin sprite drawing with transparency
+		d3ddev->BeginScene();    // begins the 3D scene
 
-											 //UI 창 렌더링 
+		d3dspt->Begin(D3DXSPRITE_ALPHABLEND);    // // begin sprite drawing with transparency
+
+												 //UI 창 렌더링 
 
 
-											 /*
-											 static int frame = 21;    // start the program on the final frame
-											 if(KEY_DOWN(VK_SPACE)) frame=0;     // when the space key is pressed, start at frame 0
-											 if(frame < 21) frame++;     // if we aren't on the last frame, go to the next frame
+												 /*
+												 static int frame = 21;    // start the program on the final frame
+												 if(KEY_DOWN(VK_SPACE)) frame=0;     // when the space key is pressed, start at frame 0
+												 if(frame < 21) frame++;     // if we aren't on the last frame, go to the next frame
 
-											 // calculate the x-position
-											 int xpos = frame * 182 + 1;
+												 // calculate the x-position
+												 int xpos = frame * 182 + 1;
+												 */
+		RECT part0;
+		SetRect(&part0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		D3DXVECTOR3 center0(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
+		D3DXVECTOR3 position0(0.0f, 0.0f, 0.0f);    // position at 50, 50 with no depth
+		d3dspt->Draw(sprite, &part0, &center0, &position0, D3DCOLOR_ARGB(255, 255, 255, 255));
 
-											 RECT part;
-											 SetRect(&part, xpos, 0, xpos + 181, 128);
-											 D3DXVECTOR3 center(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
-											 D3DXVECTOR3 position(150.0f, 50.0f, 0.0f);    // position at 50, 50 with no depth
-											 d3dspt->Draw(sprite, &part, &center, &position, D3DCOLOR_ARGB(127, 255, 255, 255));
-											 */
 
-											 //주인공 
-	RECT part;
-	SetRect(&part, 0, 0, 64, 64);
-	D3DXVECTOR3 center(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
-	D3DXVECTOR3 position(hero.x_pos, hero.y_pos, 0.0f);    // position at 50, 50 with no depth
-	d3dspt->Draw(sprite_hero, &part, &center, &position, D3DCOLOR_ARGB(255, 255, 255, 255));
+		//주인공 
+		RECT part;
+		SetRect(&part, 0, 0, 64, 64);
+		D3DXVECTOR3 center(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
+		D3DXVECTOR3 position(hero.x_pos, hero.y_pos, 0.0f);    // position at 50, 50 with no depth
+		d3dspt->Draw(sprite_hero, &part, &center, &position, D3DCOLOR_ARGB(255, 255, 255, 255));
 
-	////총알 
-	for (int i = 0; i < BULLET_NUM; i++)
-	{
-		if (bullet[i].bShow == true)
+		RECT part3;
+		SetRect(&part3, 0, 0, 64, 64);
+		D3DXVECTOR3 center3(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
+		if (hero.space == true)
 		{
-			RECT part1;
-			SetRect(&part1, 0, 0, 64, 64);
-			D3DXVECTOR3 center1(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
-			D3DXVECTOR3 position1(bullet[i].x_pos, bullet[i].y_pos, 0.0f);    // position at 50, 50 with no depth
-			d3dspt->Draw(sprite_bullet, &part1, &center1, &position1, D3DCOLOR_ARGB(255, 255, 255, 255));
+			D3DXVECTOR3 position3(hero.x_pos, hero.y_pos, 0.0f);    // position at 50, 50 with no depth
+			d3dspt->Draw(sprite_effect, &part3, &center3, &position3, D3DCOLOR_ARGB(255, 255, 255, 255));
+			
+			hNow = timeGetTime() / 1000;
+
+			if (hNow - hTime == 1.0f)         //흐른시간이 1초이상이면 내가 하고싶은것 처리
+			{
+				hero.effect_hide();
+			}
 		}
-	}
-	
+
+		////총알 
+		for (int i = 0; i < BULLET_NUM; i++)
+		{
+			if (bullet[i].bShow == true)
+			{
+				RECT part1;
+				SetRect(&part1, 0, 0, 64, 64);
+				D3DXVECTOR3 center1(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
+				D3DXVECTOR3 position1(bullet[i].x_pos, bullet[i].y_pos, 0.0f);    // position at 50, 50 with no depth
+				d3dspt->Draw(sprite_bullet, &part1, &center1, &position1, D3DCOLOR_ARGB(255, 255, 255, 255));
+			}
+		}
 
 
-	////에네미 
-	RECT part2;
-	SetRect(&part2, 0, 0, 64, 64);
-	D3DXVECTOR3 center2(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
 
-	for (int i = 0; i<ENEMY_NUM; i++)
-	{
-		D3DXVECTOR3 position2(enemy[i].x_pos, enemy[i].y_pos, 0.0f);    // position at 50, 50 with no depth
-		d3dspt->Draw(sprite_enemy, &part2, &center2, &position2, D3DCOLOR_ARGB(255, 255, 255, 255));
-	}
+		////에네미 
+		RECT part2;
+		SetRect(&part2, 0, 0, 64, 64);
+		D3DXVECTOR3 center2(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
 
+		for (int i = 0; i < ENEMY_NUM; i++)
+		{
+			D3DXVECTOR3 position2(enemy[i].x_pos, enemy[i].y_pos, 0.0f);    // position at 50, 50 with no depth
+			d3dspt->Draw(sprite_enemy, &part2, &center2, &position2, D3DCOLOR_ARGB(255, 255, 255, 255));
+		}
 
+		RECT part4;
+		SetRect(&part4, 0, 0, 64, 64);
+		D3DXVECTOR3 center4(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
 
-	d3dspt->End();    // end sprite drawing
+		eNow = timeGetTime() / 1000;
+		for (int i = 0; i < ENEMY_NUM; i++)
+		{
+			if (enemy[i].hit == true)
+			{
+				D3DXVECTOR3 position4(enemy[i].ex_pos, enemy[i].ey_pos, 0.0f);    // position at 50, 50 with no depth
+				d3dspt->Draw(sprite_effect, &part4, &center4, &position4, D3DCOLOR_ARGB(255, 255, 255, 255));
+				
+				
+				if (eNow - eTime >= 1.0f)         //흐른시간이 1초이상이면 내가 하고싶은것 처리
+				{
+					enemy[i].effect_hide();
+				}
+			}
+		}
 
-	d3ddev->EndScene();    // ends the 3D scene
+		d3dspt->End();    // end sprite drawing
 
-	d3ddev->Present(NULL, NULL, NULL, NULL);
+		d3ddev->EndScene();    // ends the 3D scene
 
-	return;
+		d3ddev->Present(NULL, NULL, NULL, NULL);
+
+		return;
+	//}
 }
 
 
@@ -838,6 +729,7 @@ void cleanD3D(void)
 	sprite_hero->Release();
 	sprite_enemy->Release();
 	sprite_bullet->Release();
+	sprite_effect->Release();
 
 	return;
 }
